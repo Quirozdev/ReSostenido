a3d6c73b0feb2c3395dfe6ba9420ac60b6c7475a
Luis Hernandez <luisernestohernandez1@hotmail.com>
2023-09-17 18:10:57 -0700

    Backend para mostrar servicios para administrar

---
+++ controllers/servicesController.js
@@ -0,0 +1,20 @@
+const db = require('../db/db');
+
+async function administrarServiciosGet(req, res) {
+    const services = await db.execute('SELECT grupo, nombre_instrumento, precio FROM servicios');
+    const groupedServices = services[0].reduce((groups, instrument) => {
+        const group = instrument.grupo;
+        if (!groups[group]) {
+          groups[group] = [];
+        }
+        groups[group].push(instrument);
+        return groups;
+      }, {});
+
+
+    grups = Object.keys(groupedServices); 
+    console.log(grups);
+    res.render('administrar_servicios.html', { servicios: groupedServices, grupos: grups});
+}
+module.exports = { administrarServiciosGet};
+
--- routes/services.js
+++ routes/services.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const servicesController = require('../controllers/servicesController');
 
 
 router.get('/administrar_servicios', 
-    (req, res) => {
-        res.render('administrar_servicios.html');
-    });
+    servicesController.administrarServiciosGet);
 
 module.exports = router;
