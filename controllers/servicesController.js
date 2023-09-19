e3655f92b5df700528679434714e2d722bfbbca2
Quirozdev <luisdevv232@gmail.com>
2023-09-18 21:52:31 -0700

    add routing and template rendering for user services page

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -16,5 +16,22 @@ async function administrarServiciosGet(req, res) {
     console.log(grups);
     res.render('administrar_servicios.html', { servicios: groupedServices, grupos: grups});
 }
-module.exports = { administrarServiciosGet};
+
+async function serviciosGet(req, res) {
+  const services = await db.execute('SELECT id, grupo, nombre_instrumento, precio FROM servicios');
+    const groupedServices = services[0].reduce((groups, instrument) => {
+        const group = instrument.grupo;
+        if (!groups[group]) {
+          groups[group] = [];
+        }
+        groups[group].push(instrument);
+        return groups;
+      }, {});
+
+
+    grups = Object.keys(groupedServices); 
+    res.render('servicios.html', { servicios: groupedServices, grupos: grups});
+}
+
+module.exports = { administrarServiciosGet, serviciosGet };
 
--- routes/services.js
+++ routes/services.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const servicesController = require('../controllers/servicesController');
 
+router.get(
+  '/administrar_servicios',
+  servicesController.administrarServiciosGet
+);
 
-router.get('/administrar_servicios', 
-    servicesController.administrarServiciosGet);
+router.get('/servicios', servicesController.serviciosGet);
 
 module.exports = router;
