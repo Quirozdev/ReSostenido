c49283efaf32fb2b9e0c008196dce8dca1e162bb
AdrianVargas23 <adrianpatata23@live.com>
2023-09-30 17:35:53 -0700

    Implementación de fixtures
    Test actualizados con fixtures, creación de test para Agregar Servicio.

--- cypress/e2e/Producción/CU-Eliminar_Servicio.cy.js
+++ cypress/e2e/Producción/CU-Eliminar_Servicio.cy.js
@@ -2,8 +2,13 @@ describe('Eliminar Servicio', () => {
   before(() => {
     // Realizar la visita al sitio solo una vez al comienzo de la prueba
     cy.visit('https://resostenido-production.up.railway.app/login')
-    cy.get('#email').type('adrianvargasuson@gmail.com', { force: true })
-    cy.get('#contrasenia').type('TacosDeGansito', { force: true })
+
+    // Cargar datos desde el archivo de fixture 'registro.json' para el inicio de sesión
+    cy.fixture('registro.json').then((registro) => {
+      cy.get('#email').type(registro.email, { force: true });
+      cy.get('#contrasenia').type(registro.contrasena, { force: true });
+    });
+
     cy.get('#login').click()
   })
 
@@ -26,7 +31,7 @@ describe('Eliminar Servicio', () => {
     //cy.contains('Servicios inactivos').next().contains('Floyd Rose').should('be.visible');
 
     //Espera
-    cy.wait(10000);
+    cy.wait(5000);
 
     //Habilitar de nuevo el servicio
     cy.contains('Floyd Rose').parent().find('button[type="submit"]').click({force:true});
