6076d4b9aafc9998c578a2bd0e7cfabfbf6b609e
Quirozdev <luisdevv232@gmail.com>
2023-10-22 14:44:41 -0700

    little changes to the communication between frontend and backend in agendar-cita

--- middlewares/validations/agendarCitaValidation.js
+++ middlewares/validations/agendarCitaValidation.js
@@ -1,7 +1,7 @@
 const { body } = require('express-validator');
 
 const validateAgendarCita = [
-  body('idServicio', 'Id de servicio invalida')
+  body('id_servicio', 'Id de servicio invalida')
     .trim()
     .escape()
     .notEmpty()
@@ -18,16 +18,23 @@ const validateAgendarCita = [
     .escape()
     .notEmpty()
     .withMessage('La hora no puede estar vacia')
+    .customSanitizer((value) => {
+      // para quitar los segundos
+      return value.split(':', 2).join(':');
+    })
     .isTime({
       hourFormat: 'hour24',
     }),
-  body('motivo', 'Motivo inválido')
+  body('descripcion', 'Descripción inválida')
     .trim()
     .escape()
     .notEmpty()
-    .withMessage('El motivo no puede estar vacío')
+    .withMessage('La descripción no puede estar vacío')
     .isLength({ max: 255 })
-    .withMessage('El motivo debe tener 255 caracteres o menos'),
+    .withMessage('La descripción debe tener 255 caracteres o menos'),
+  body('incluye_cuerdas').customSanitizer((value) => {
+    return value === 'true' ? true : false;
+  }),
 ];
 
 module.exports = { validateAgendarCita };
