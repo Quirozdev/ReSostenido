d92736580daf0252e0e79291c48365526b181dac
Julio <juliosanchezd26@gmail.com>
2023-11-26 21:48:04 -0700

    test produccion info citas

--- cypress/e2e/Producción/CU-Consultar_info_citas_cliente.cy.js
+++ cypress/e2e/Producción/CU-Consultar_info_citas_cliente.cy.js
@@ -1,31 +1,55 @@
 describe('Pagina de inicio de sesion', () => {
-    before(() => {
-        // Deshabilitar la detección de errores no atrapados
-        Cypress.on('uncaught:exception', (err, runnable) => {
-          // evita que Cypress falle por errores no atrapados
-          return false;
-        });
-    
-        // Visitar la página
-        cy.visit('https://resostenidoclone-production-13cb.up.railway.app/');
+  before(() => {
+      // Deshabilitar la detección de errores no atrapados
+      Cypress.on('uncaught:exception', (err, runnable) => {
+        // evita que Cypress falle por errores no atrapados
+        return false;
       });
   
-    it('Encontrar el boton de menu', () => {
-      // Click en iniciar sesión
-      cy.get('#loginNavItem > .nav-link').should('be.visible').click()
-      
-      // Cargar datos de inicio de sesión desde el archivo de fixture 'registro.json'
-      cy.fixture('datos_cliente.json').then((datos) => {
-        cy.get('#email').should('be.visible').type(datos.email, { force: true });
-        cy.get('#contrasenia').should('be.visible').type(datos.contrasena, { force: true });
-      });
-      
-      //Iniciar sesión
-      cy.get('#login').should('be.visible').click()
+      // Visitar la página
+      cy.visit('https://resostenidoclone-production-13cb.up.railway.app/login');
+    });
 
-      // Ir al apartado de citas
-      cy.get('#citasNavItem > .nav-link').should('be.visible').click()
+  it('Encontrar el boton de menu', () => {
+    // Click en iniciar sesión
+    cy.get('#loginNavItem > .nav-link').should('be.visible').click()
+    
+    // Cargar datos de inicio de sesión desde el archivo de fixture 'registro.json'
+    cy.fixture('datos_admin.json').then((datos) => {
+      cy.get('#email').should('be.visible').type(datos.email, { force: true });
+      cy.get('#contrasenia').should('be.visible').type(datos.contrasena, { force: true });
+    });
+    
+    //Iniciar sesión
+    cy.get('#login').should('be.visible').click()
 
-      // TODO: consultar informacion
-    })
+    // Ir al apartado de citas
+    cy.get('#citasNavItem > .nav-link').should('be.visible').click()
+
+    // ver detalles de la cita
+    cy.get('#btnverCita').click({force:true})
+    
+    // Verificar que aparezca nombre del instrumento, descripcion, costo total y fecha
+    cy.fixture('datos_cliente.json').then((datos) => {
+      // nombre instrumento
+      cy.get(':nth-child(4) > .row > .table > tbody > :nth-child(1) > .line-placeholder').should('contain.text', datos.nombre_instrumento)
+      // descripcion servicio
+      cy.get(':nth-child(2) > .border > .row > .table > tbody > :nth-child(2) > .line-placeholder').should('contain.text', datos.descripcion)
+      // fecha y hora
+      cy.get(':nth-child(2) > .border > .row > .table > tbody > :nth-child(3) > :nth-child(1)').should('contain.text', "Fecha y hora")
+      // X no aparece el anticipo
+      //cy.get(':nth-child(4) > .line-placeholder').should('contain.text', datos.anticipo)
+      // nota cliente
+      cy.get('tbody > :nth-child(5) > :nth-child(1)').should('contain.text', "Notas del cliente")
+      // incluye cuerdas
+      cy.get('tbody > :nth-child(6) > :nth-child(1)').should('contain.text', "Incluye cuerdas")
+      // estado
+      cy.get(':nth-child(2) > .border > .row > .table > tbody > :nth-child(4) > :nth-child(1)').should('contain.text', "Estado de la cita")
+      // Cancelar cita
+      cy.get('#cancelar-cita-form > .d-flex > .me-2 > .btn').click({force:true})
+      // debe de decir cancelada
+      cy.get(':nth-child(2) > .border > .row > .table > tbody > :nth-child(4) > .line-placeholder').should('contain.text', "Cancelada")
+
+    });
   })
+})
